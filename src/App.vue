bab88a5e0002db812dc24a78a140d2e7c09dc5f4
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-01-30 16:53:55 -0300

    Commit inicial subindo o projeto base

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -22,7 +22,7 @@ nav {
 nav a {
   font-weight: bold;
   color: #2c3e50;
-}
+} */
 
 nav a.router-link-exact-active {
   color: #42b983;
